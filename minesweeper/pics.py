b45ac4202dffd270f913cc911401863bf25657fb
Sam Pfeiffer <sampfeiffer54+git@gmail.com>
2017-11-12 19:31:43 -0500

    Generalized path of pics to be agnostic to operating systems

--- minesweeper/pics.py
+++ minesweeper/pics.py
@@ -2,17 +2,18 @@ import os
 import pygame
 
 class Pics():
-    path = os.path.dirname(os.path.abspath(__file__)) + '/pics/'
-    bluemine = pygame.image.load(path + 'MinesweeperBlueMine.jpg')
-    clock = pygame.image.load(path + 'MinesweeperClock.jpg')
-    smiley = pygame.image.load(path + 'MinesweeperSmiley.jpg')
-    uhoh = pygame.image.load(path + 'MinesweeperUhOh.jpg')
-    sad = pygame.image.load(path + 'MinesweeperSad.jpg')
-    sunglasses = pygame.image.load(path + 'MinesweeperSunglass.jpg')
-    flag_scroll = pygame.image.load(path + 'MinesweeperFlagScroll.jpg')
-    flag = pygame.image.load(path + 'MinesweeperFlag.jpg')
-    mine = pygame.image.load(path + 'Mine.jpg')
-    red_mine = pygame.image.load(path + 'RedMine.jpg')
-    flag_mine = pygame.image.load(path + 'MineWithFlag.jpg')
-    flag_x = pygame.image.load(path + 'MinesweeperFlagX.jpg')
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pics')
+    
+    bluemine = pygame.image.load(os.path.join(path, 'MinesweeperBlueMine.jpg'))
+    clock = pygame.image.load(os.path.join(path, 'MinesweeperClock.jpg'))
+    smiley = pygame.image.load(os.path.join(path, 'MinesweeperSmiley.jpg'))
+    uhoh = pygame.image.load(os.path.join(path, 'MinesweeperUhOh.jpg'))
+    sad = pygame.image.load(os.path.join(path, 'MinesweeperSad.jpg'))
+    sunglasses = pygame.image.load(os.path.join(path, 'MinesweeperSunglass.jpg'))
+    flag_scroll = pygame.image.load(os.path.join(path, 'MinesweeperFlagScroll.jpg'))
+    flag = pygame.image.load(os.path.join(path, 'MinesweeperFlag.jpg'))
+    mine = pygame.image.load(os.path.join(path, 'Mine.jpg'))
+    red_mine = pygame.image.load(os.path.join(path, 'RedMine.jpg'))
+    flag_mine = pygame.image.load(os.path.join(path, 'MineWithFlag.jpg'))
+    flag_x = pygame.image.load(os.path.join(path, 'MinesweeperFlagX.jpg'))
 

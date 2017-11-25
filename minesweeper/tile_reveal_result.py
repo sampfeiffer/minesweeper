a6db0d3dc5a939ac7786df6ee3cd379484bd7943
Sam Pfeiffer <sampfeiffer54+git@gmail.com>
2017-11-25 18:51:32 -0500

    Avoid mutable default argument

--- minesweeper/tile_reveal_result.py
+++ minesweeper/tile_reveal_result.py
@@ -4,17 +4,18 @@ class TileRevealResult:
     This is useful for combining the results of a shortcut click.
     '''
 
-    def __init__(self, non_mines_uncovered=0, hit_mine=False, mine_tiles=[]):
+    def __init__(self, non_mines_uncovered=0, hit_mine=False, mine_tiles=None):
         '''
         Args:
             non_mines_uncovered (int): The number of mines uncovered by the click. Defaults to 0.
             hit_mine (bool): Did the click hit a mine? Defaults to False.
-            mine_tiles (list<Tile>): A list of tiles clicked that contain a mine. Defaults to an empty list.
+            mine_tiles (list<Tile>|None): A list of tiles clicked that contain a mine or None if no tiles clicked
+                contained a mine. Defaults to None.
         '''
 
         self.non_mines_uncovered = non_mines_uncovered
         self.hit_mine = hit_mine
-        self.mine_tiles = mine_tiles
+        self.mine_tiles = [] if mine_tiles is None else mine_tiles
 
     def __add__(self, other):
         '''
